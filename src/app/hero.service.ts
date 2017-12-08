8d63fe6633301760aa0e644f972ce9981bfc8a99
Sontaga <sonti96@live.co.uk>
2017-12-08 16:35:46 +0200

    Services implemented and working as they should

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import  {Observable} from ''
+import  {Observable} from 'rxjs/Observable';
+import {Hero} from './hero';
 import {of} from 'rxjs/observable/of';
-
 import {MessageService} from './message.service';
+import {HEROES} from './mock-heroes';
+
 @Injectable()
 export class HeroService {
-
-  constructor() { }
+  constructor(private messageService:MessageService) { }
   getHeroes():Observable<Hero[]>{
     //of(HEROES) will return an OBSERVABLE<Hero[]> emits a single value(array of mock heroes)
+    this.messageService.add('HeroService:fetched heroes');
     return of(HEROES);
   }
 }
